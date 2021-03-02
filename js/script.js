f53d7db428945bc0015f985b31689ec6677a399d
Arun G <garunalways@gmail.com>
2021-03-02 06:19:54 +0530

    Time correction on timer

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("April 24, 2021 00:00:00").getTime();
+var countDownDate = new Date("April 24, 2021 12:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
